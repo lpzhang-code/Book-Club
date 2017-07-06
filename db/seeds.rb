3dc18ac79d0f0fc3bb960eba0c832e09d29cddba
Peter Zhang <lpzhang@me.com>
2017-07-06 17:21:50 +1000

    Seed sample users and books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |n|
+    uid = Faker::Number.number(10)
+    name = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    gravatar_id = Digest::MD5::hexdigest(email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    thoughts = Faker::Lorem.sentence(3)
+    User.create(uid: uid, name: name, image_url: gravatar_url, thoughts: thoughts)
+end
+
+@users = User.all
+
+@users.each do |user|
+    1.times do
+        title = Faker::Book.title
+        @books = GoogleBooks.search(title, {count: 3})
+        @books.each do |book|
+            user.books.create(title: book.title, author: book.authors, pages: book.page_count, info: book.info_link, image: book.image_link, rating: book.ratings_count)
+        end
+    end
+end
