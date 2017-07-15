0e99e734b34e108dd67b8de651d0f8be76a9191d
Peter Zhang <lpzhang@me.com>
2017-07-15 21:36:36 +1000

    Add feed for logged in user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
     def index
+        if current_user
+            following_ids = current_user.following_ids
+            @books = Book.where("user_id IN(?)", following_ids).paginate(page: params[:page], per_page: 6)
+        end
     end
     
     def post
