da1658d24bf33b14a05752a923a223a122aa0327
Peter Zhang <lpzhang@me.com>
2017-07-17 22:34:34 +1000

    Create following/followers page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,4 +21,18 @@ class PagesController < ApplicationController
         @user = User.find(params[:id])
         @books = @user.books.paginate(page: params[:page], per_page: 4)
     end
+    
+    def following
+        @title = "Following"
+        @user = User.find(params[:id])
+        @users = @user.following
+        render 'show_follow'
+    end
+    
+    def followers
+        @title = "Followers"
+        @user = User.find(params[:id])
+        @users = @user.followers
+        render 'show_follow'
+    end
 end
