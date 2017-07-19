8bbcb206618ba3f0c97d1e668c7f194b7bf05e15
Peter Zhang <lpzhang@me.com>
2017-07-19 22:35:43 +1000

    Add authorisations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :authenticated_user, only: [:post, :thoughts, :book, :relationships]
     
     private
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
     
+    def authenticated_user
+        if session[:user_id] == nil
+            flash[:danger] = 'You need to log in'
+            redirect_to root_path
+        end
+    end
     helper_method :current_user
 end
