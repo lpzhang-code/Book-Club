f59e226c17aa37f012b7b78406f04e1ceccef7e9
Peter Zhang <lpzhang@me.com>
2021-05-26 12:51:00 +1000

    Update App ID and Secrets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1027300150662049', '680f5dc0c2835c2bf03f8c759d982f18',
+  provider :facebook, secrets.APP_ID, secrets.APP_SECRET,
     scope: 'public_profile', info_fields: 'id,name,link'
 end
