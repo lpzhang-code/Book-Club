4df6d30c9291ffb53ed4a24d5da2c0af669dc705
Peter Zhang <lpzhang@me.com>
2021-05-26 12:58:51 +1000

    Remove unnecessary files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, secrets.APP_ID, secrets.APP_SECRET,
+  provider :facebook, '591109025188461', 'cc55a675f1234b455a99dcb2c6648752',
     scope: 'public_profile', info_fields: 'id,name,link'
 end
